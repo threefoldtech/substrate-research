8375250e9d98e2e8e516595decf8ce9b4ebac539
LeeSmet <lee.smet@hotmail.com>
2020-08-28 14:40:49 +0200

    Implement set metadata method in pallet

--- 0-stor/substrate/pallets/template/src/lib.rs
+++ 0-stor/substrate/pallets/template/src/lib.rs
@@ -8,9 +8,11 @@
 
 /// For more guidance on Substrate FRAME, see the example pallet
 /// https://github.com/paritytech/substrate/blob/master/frame/example/src/lib.rs
-
-use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch};
+use frame_support::{
+    decl_error, decl_event, decl_module, decl_storage, dispatch, ensure, Hashable,
+};
 use frame_system::{self as system, ensure_signed};
+use sp_std::vec::Vec;
 
 #[cfg(test)]
 mod mock;
@@ -20,90 +22,83 @@ mod tests;
 
 /// The pallet's configuration trait.
 pub trait Trait: system::Trait {
-	// Add other types and constants required to configure this pallet.
+    // Add other types and constants required to configure this pallet.
 
-	/// The overarching event type.
-	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
+    /// The overarching event type.
+    type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
 }
 
 // This pallet's storage items.
 decl_storage! {
-	// It is important to update your storage name so that your pallet's
-	// storage items are isolated from other pallets.
-	// ---------------------------------vvvvvvvvvvvvvv
-	trait Store for Module<T: Trait> as TemplateModule {
-		// Just a dummy storage item.
-		// Here we are declaring a StorageValue, `Something` as a Option<u32>
-		// `get(fn something)` is the default getter which returns either the stored `u32` or `None` if nothing stored
-		Something get(fn something): Option<u32>;
-	}
+    // It is important to update your storage name so that your pallet's
+    // storage items are isolated from other pallets.
+    // ---------------------------------vvvvvvvvvvvvvv
+    trait Store for Module<T: Trait> as TemplateModule {
+        /// Storage backend for 0-stor.
+        ///
+        /// Key is the blake2b_128 hash of the namespace + plain key bytes
+        /// Item is a tuple, first element is the encoded metadata, second item is the account
+        /// ID which originally inserted the metadata. Only this account can later update or delete
+        /// it.
+        MetaStor get(fn meta_stor): map hasher(identity) Vec<u8> => (Vec<u8>, T::AccountId);
+    }
 }
 
 // The pallet's events
 decl_event!(
-	pub enum Event<T> where AccountId = <T as system::Trait>::AccountId {
-		/// Just a dummy event.
-		/// Event `Something` is declared with a parameter of the type `u32` and `AccountId`
-		/// To emit this event, we call the deposit function, from our runtime functions
-		SomethingStored(u32, AccountId),
-	}
+    pub enum Event<T>
+    where
+        AccountId = <T as system::Trait>::AccountId,
+    {
+        /// New metadata was inserted in a namespace. [key, who]
+        MetadataCreated(AccountId, Vec<u8>),
+    }
 );
 
 // The pallet's errors
 decl_error! {
-	pub enum Error for Module<T: Trait> {
-		/// Value was None
-		NoneValue,
-		/// Value reached maximum and cannot be incremented further
-		StorageOverflow,
-	}
+    pub enum Error for Module<T: Trait> {
+        /// Tried to insert metadata in a namespace with a key which already exists in said
+        /// namespace.
+        MetadataExists,
+    }
 }
 
 // The pallet's dispatchable functions.
 decl_module! {
-	/// The module declaration.
-	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-		// Initializing errors
-		// this includes information about your errors in the node's metadata.
-		// it is needed only if you are using errors in your pallet
-		type Error = Error<T>;
+    /// The module declaration.
+    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+        // Initializing errors
+        // this includes information about your errors in the node's metadata.
+        // it is needed only if you are using errors in your pallet
+        type Error = Error<T>;
 
-		// Initializing events
-		// this is needed only if you are using events in your pallet
-		fn deposit_event() = default;
+        // Initializing events
+        // this is needed only if you are using events in your pallet
+        fn deposit_event() = default;
 
-		/// Just a dummy entry point.
-		/// function that can be called by the external world as an extrinsics call
-		/// takes a parameter of the type `AccountId`, stores it, and emits an event
-		#[weight = 10_000]
-		pub fn do_something(origin, something: u32) -> dispatch::DispatchResult {
-			// Check it was signed and get the signer. See also: ensure_root and ensure_none
-			let who = ensure_signed(origin)?;
+        /// Set metadata in a given namespace with a given key. Ownership of the metadata is
+        /// assigned to the caller of the function. It is an error to set metadata on a key if that
+        /// key is already in use in the namespace, even if the previous key is owned by the
+        /// calling user.
+        #[weight = 10_000] // TODO
+        pub fn set_metadata(origin, namespace: Vec<u8>, key: Vec<u8>, metadata: Vec<u8>) -> dispatch::DispatchResult {
+            let sender = ensure_signed(origin)?;
 
-			// Code to execute when something calls this.
-			// For example: the following line stores the passed in u32 in the storage
-			Something::put(something);
+            // construct key
+            let namespace_hash = &namespace.blake2_128();
+            let mut hashed_key: Vec<u8>= namespace_hash[..].into();
+            hashed_key.extend_from_slice(&key);
+            // hashed_key is now namespace_hash + raw key bytes
 
-			// Here we are raising the Something event
-			Self::deposit_event(RawEvent::SomethingStored(something, who));
-			Ok(())
-		}
+            // Makse sure this is not an update, there is a separate function for that
+            ensure!(!MetaStor::<T>::contains_key(&hashed_key), Error::<T>::MetadataExists);
 
-		/// Another dummy entry point.
-		/// takes no parameters, attempts to increment storage value, and possibly throws an error
-		#[weight = 10_000]
-		pub fn cause_error(origin) -> dispatch::DispatchResult {
-			// Check it was signed and get the signer. See also: ensure_root and ensure_none
-			let _who = ensure_signed(origin)?;
+            MetaStor::<T>::insert(&hashed_key, (&metadata, &sender));
 
-			match Something::get() {
-				None => Err(Error::<T>::NoneValue)?,
-				Some(old) => {
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					Something::put(new);
-					Ok(())
-				},
-			}
-		}
-	}
+            Self::deposit_event(RawEvent::MetadataCreated(sender, key));
+
+            Ok(())
+        }
+    }
 }
